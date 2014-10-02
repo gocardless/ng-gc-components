4c82dbce10dd62d1692ff7cbe842867c751e311f
Jack Franklin <jack@jackfranklin.net>
2014-10-02 16:53:24 +0100

    Update to angular 1.3

--- ng-gc-upload-form-directive/gc-upload-form-controller.js
+++ ng-gc-upload-form-directive/gc-upload-form-controller.js
@@ -1,30 +1,16 @@
 'use strict';
 
-angular.module('gc.uploadFormController', [])
-  .controller('GcUploadFormController', [
-    '$scope',
-    function GcUploadFormController($scope) {
-
-      /**
-       * @param  {Object|Null} response
-       * @return {Array|Undefined}
-       */
-      function parseErrors(response) {
-        if (!response) { return; }
-        // rails is a dick
-        var errors = response.error || response.errors;
-        if (errors == null) { return; }
-        return !_.isObject(errors) ? [errors] :
-          _.uniq(_.flatten(_.values(errors)));
-      }
-
-      $scope.handleUploadEnd = function handleUpload(response) {
-        $scope.uploadErrors = parseErrors(response);
-        $scope.onUploadEnd({
-          errors: $scope.uploadErrors,
-          response: response
-        });
-      };
-
-    }
-  ]);
+angular.module('gc.uploadFormController', [
+  'gc.railsErrorNormalizer'
+]).controller('GcUploadFormController', [
+  '$scope', 'RailsErrorNormalizerService',
+  function GcUploadFormController($scope, RailsErrorNormalizerService) {
+    $scope.handleUploadEnd = function handleUpload(response) {
+      $scope.uploadErrors = RailsErrorNormalizerService.normalize(response);
+      $scope.onUploadEnd({
+        errors: $scope.uploadErrors,
+        response: response
+      });
+    };
+  }
+]);
